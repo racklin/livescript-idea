b12a5b57c928756a83d14246d5f5b617eaeeb79f
Rack Lin <racklin@gmail.com>
2013-06-19 03:03:39 +0800

    Rename LiveScriptFlexLexer to LiveScriptLexerAdapter Signed-off-by: Rack Lin <racklin@gmail.com>

--- src/org/livescriptidea/lang/lexer/LiveScriptLexerAdapter.java
+++ src/org/livescriptidea/lang/lexer/LiveScriptLexerAdapter.java
@@ -3,15 +3,38 @@ package org.livescriptidea.lang.lexer;
 import com.intellij.lexer.FlexAdapter;
 
 import java.io.Reader;
+
 /**
- * Created with IntelliJ IDEA.
- * User: rack
- * Date: 13/6/19
- * Time: AM2:54
- * To change this template use File | Settings | File Templates.
+ * Adapter for the LiveScript flex lexer
+ *
+ * @author Rack Lin
+ * @since 0.1.0
  */
 public class LiveScriptLexerAdapter extends FlexAdapter {
-    public LiveScriptLexerAdapter() {
-        super(new LiveScriptLexer((Reader) null));
+
+  public LiveScriptLexerAdapter() {
+    super(new LiveScriptLexer((Reader) null));
+  }
+
+  /**
+   * This start implementation first advances the lexer from the start of the file to the start offset, so
+   * that the complex state stack is reconstructed.
+   *
+   * @param buffer The content to lex
+   * @param startOffset The start offset
+   * @param endOffset The end offset
+   * @param initialState The state to start with
+   */
+  public void start(final CharSequence buffer, int startOffset, int endOffset, final int initialState) {
+    if (startOffset != 0) {
+      super.start(buffer, 0, startOffset, initialState);
+
+      while (getTokenType() != null) {
+        advance();
+      }
     }
+
+    super.start(buffer, startOffset, endOffset, getState());
+  }
+
 }
